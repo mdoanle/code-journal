0b071372a14e443430acb3f6f5804fea3d48eebe
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-13 20:43:55 +0000

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/data.js
+++ js/data.js
@@ -7,8 +7,43 @@ var data = {
   nextEntryId: 1
 };
 
+var journalEntryJSON = localStorage.getItem('Journal Entry');
+if (journalEntryJSON !== null) {
+  data = JSON.parse(journalEntryJSON);
+}
+
 window.addEventListener('beforeunload', handleBeforeUnload);
 function handleBeforeUnload(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('Journal Entry', dataJSON);
 }
+
+function renderEntry(data) {
+  var liRow = document.createElement('li');
+  liRow.setAttribute('class', 'row');
+
+  var divForImg = document.createElement('div');
+  divForImg.setAttribute('class', 'column-half');
+  liRow.appendChild(divForImg);
+
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('alt', 'journal-image');
+  entryImg.setAttribute('src', data.entries[0].photoUrl);
+  divForImg.appendChild(entryImg);
+
+  var divForSection = document.createElement('div');
+  divForSection.setAttribute('class', 'column-half-right');
+  liRow.appendChild(divForSection);
+
+  var headerSection = document.createElement('h2');
+  headerSection.textContent = data.entries[0].title;
+  divForSection.appendChild(headerSection);
+
+  var textSection = document.createElement('p');
+  textSection.textContent = data.entries[0].notesInput;
+  divForSection.appendChild(textSection);
+
+  return liRow;
+}
+var $ulElement = document.querySelector('.entry-list');
+$ulElement.appendChild(renderEntry(data));
