d9900b7c2048da63dc5452dc02349f3170907bcf
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-14 17:54:31 +0000

    Test the app some more in a private browsing tab. #2

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-var $formEntry = document.querySelector('form');
+
 var journalEntryJSON = localStorage.getItem('Journal Entry');
 if (journalEntryJSON !== null) {
   data = JSON.parse(journalEntryJSON);
@@ -16,54 +16,4 @@ window.addEventListener('beforeunload', handleBeforeUnload);
 function handleBeforeUnload(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('Journal Entry', dataJSON);
-}
-
-function renderEntry(data) {
-  var liRow = document.createElement('li');
-  liRow.setAttribute('class', 'row');
-
-  var divForImg = document.createElement('div');
-  divForImg.setAttribute('class', 'column-half');
-  liRow.appendChild(divForImg);
-
-  var entryImg = document.createElement('img');
-  entryImg.setAttribute('alt', 'journal-image');
-  entryImg.setAttribute('src', data.photoUrl);
-  divForImg.appendChild(entryImg);
-
-  var divForSection = document.createElement('div');
-  divForSection.setAttribute('class', 'column-half-right');
-  liRow.appendChild(divForSection);
-
-  var sectionForText = document.createElement('section');
-  divForSection.appendChild(sectionForText);
-
-  var headerSection = document.createElement('h2');
-  var h2TextContent = document.createTextNode(data.title);
-  headerSection.appendChild(h2TextContent);
-  sectionForText.appendChild(headerSection);
-
-  var textSection = document.createElement('p');
-  var pTextContent = document.createTextNode(data.notesInput);
-  textSection.appendChild(pTextContent);
-  sectionForText.appendChild(textSection);
-
-  return liRow;
-}
-
-$formEntry.addEventListener('submit', handleSubmit2);
-function handleSubmit2(event) {
-  var $ulList = document.querySelector('.entry-list');
-  var journalEntryLoop = renderEntry(data.entries[0]);
-  $ulList.appendChild(journalEntryLoop);
-
-}
-
-window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-function handleDOMContentLoaded(event) {
-  var $ulList = document.querySelector('.entry-list');
-  for (var i = 1; i < data.entries.length; i++) {
-    var journalEntryLoop = renderEntry(data.entries[i]);
-    $ulList.appendChild(journalEntryLoop);
-  }
 }
--- js/main.js
+++ js/main.js
@@ -27,7 +27,56 @@ function handleSubmit(event) {
   data.entries.unshift(formObj);
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $formEntry.reset();
+}
+
+function renderEntry(data) {
+  var liRow = document.createElement('li');
+  liRow.setAttribute('class', 'row');
+
+  var divForImg = document.createElement('div');
+  divForImg.setAttribute('class', 'column-half');
+  liRow.appendChild(divForImg);
+
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('alt', 'journal-image');
+  entryImg.setAttribute('src', data.photoUrl);
+  divForImg.appendChild(entryImg);
+
+  var divForSection = document.createElement('div');
+  divForSection.setAttribute('class', 'column-half right');
+  liRow.appendChild(divForSection);
+
+  var sectionForText = document.createElement('section');
+  divForSection.appendChild(sectionForText);
+
+  var headerSection = document.createElement('h2');
+  var h2TextContent = document.createTextNode(data.title);
+  headerSection.appendChild(h2TextContent);
+  sectionForText.appendChild(headerSection);
+
+  var textSection = document.createElement('p');
+  var pTextContent = document.createTextNode(data.notesInput);
+  textSection.appendChild(pTextContent);
+  sectionForText.appendChild(textSection);
+
+  return liRow;
+}
+
+$formEntry.addEventListener('submit', handleSubmit2);
+function handleSubmit2(event) {
   handleClick2();
+  var $ulList = document.querySelector('.entry-list');
+  var journalEntry = renderEntry(data.entries[0]);
+  $ulList.prepend(journalEntry);
+}
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+function handleDOMContentLoaded(event) {
+  var $ulList = document.querySelector('.entry-list');
+  for (var i = 0; i < data.entries.length; i++) {
+    var journalEntryLoop = renderEntry(data.entries[i]);
+    $ulList.appendChild(journalEntryLoop);
+  }
 }
 
 $viewSwapButton.addEventListener('click', handleClick);
